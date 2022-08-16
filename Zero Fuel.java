64b16b1c51ea1bac53f3c1a3eefdaf4f6778ae5c
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2022-08-16 11:50:10 +0200

    Create Zero Fuel.java

---
+++ Zero Fuel.java
@@ -0,0 +1,7 @@
+public class Kata {
+  
+  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+    return distanceToPump <= mpg * fuelLeft ? true : false;
+  }
+  
+}
