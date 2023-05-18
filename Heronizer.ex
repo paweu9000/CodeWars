5e8d165487e2e38345b169390e65b66d97120423
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2023-05-18 10:56:06 +0200

    Update Heronizer.ex

--- Heronizer.ex
+++ Heronizer.ex
@@ -1,17 +1,5 @@
 defmodule Codewars.Heronizer do
-  def no_boring_zeros(0) do
-    0
-  end
-  
-  def no_boring_zeros(n) do
-    str_n = Integer.to_string(n)
-    delete_zero(str_n)
-  end
-  
-  def delete_zero(n) do
-    cond do
-      String.ends_with?(n, "0") -> delete_zero(String.trim_trailing(n, "0"))
-      true -> String.to_integer(n)
-    end
-  end
+  def no_boring_zeros(0), do: 0
+  def no_boring_zeros(n) when rem(n, 10) == 0, do: no_boring_zeros(div(n, 10))
+  def no_boring_zeros(n), do: n
 end
